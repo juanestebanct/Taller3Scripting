3a291bfbe859b768a66a9962e8565b0484109f40
Nicolás <83988504+The-AshLord@users.noreply.github.com>
2022-05-05 22:09:23 -0500

    Seleción en el Gun Controller

--- Assets/Scripting/Scene3/Bullet.cs
+++ Assets/Scripting/Scene3/Bullet.cs
@@ -7,8 +7,8 @@ public class Bullet : MonoBehaviour
     //nolaestoypasandobienauxilio
 
     [SerializeField] private float autoDestroyTime;
-    //Velocidad de la bala:
-    [SerializeField] private float speed = 30.0f;
+    
+    
 
     private Rigidbody rb;
 
@@ -32,14 +32,6 @@ public class Bullet : MonoBehaviour
         
     }
 
-    private void BulletMovement(GameObject bullet)
-    {
-        Vector3 bulletVelocity = Vector3.right * speed;                 //Set Bullet Direction
-        bullet.transform.Translate(bulletVelocity * Time.deltaTime);   //Move Bullet based on Time
-        //Rigidbody3D rb = bullet.GetComponent<Rigidbody3D>();
-        //rb.AddForce(FirePoint.up * bulletForce, ForceMode3D.Impulse);
-
-    }
     internal virtual void CollisionEffects()
     {
 
--- Assets/Scripting/Scene3/GunController.cs
+++ Assets/Scripting/Scene3/GunController.cs
@@ -6,34 +6,64 @@ public class GunController : MonoBehaviour
 {
     //Punto de disparo de las balas:
     public Transform FirePoint;
-    
+    //Velocidad de la bala:
+    [SerializeField] private float speed = 30.0f;
+
     //Prefabs de los 3 tipos de balas
     public GameObject TextBullet;
     public GameObject BlockBullet;
     public GameObject ParticleBullet;
+
+    private GameObject ActualBullet;
 
     //Referencias a los pools
     private BlockBulletPool blockBulletPool = new BlockBulletPool();
     private ParticleBulletPool particleBulletPool = new ParticleBulletPool();
     private TextBulletPool textBulletPool = new TextBulletPool();
 
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        ActualBullet = TextBullet;
     }
 
-    private void BulletSelector()
+
+    void Update()
     {
+        BulletSelector();
+    }
+
+    private void BulletSelector() 
+    {
+
+        //Revisar que cambios hay que hacer al implementar los pools
         if (Input.GetMouseButtonDown(0))
         {
-            
+            BulletMovement(ActualBullet);
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            
+            if (ActualBullet == TextBullet)
+            {
+                ActualBullet = BlockBullet;
+            }
+            else if (ActualBullet == BlockBullet)
+            {
+                ActualBullet = ParticleBullet;
+            }
+            else if (ActualBullet = ParticleBullet)
+            {
+                ActualBullet = TextBullet;
+            }
+
         }
+
+    }
+    private void BulletMovement(GameObject bullet)
+    {
+        Vector3 bulletVelocity = Vector3.right * speed;                 //Set Bullet Direction
+        bullet.transform.Translate(bulletVelocity * Time.deltaTime);   //Move Bullet based on Time
+        //Rigidbody3D rb = bullet.GetComponent<Rigidbody3D>();
+        //rb.AddForce(FirePoint.up * bulletForce, ForceMode3D.Impulse);
 
     }
 }
